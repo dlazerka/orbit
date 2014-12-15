8bb9ef4c9d1e643dc8319a6cf531515103b53f9f
Dzmitry Lazerka <dlazerka@gmail.com>
2014-12-14 18:35:16 -0800

    Moved libs to lib/

--- gae/src/main/webapp/orbit.js
+++ gae/src/main/webapp/orbit.js
@@ -2,7 +2,8 @@
 angular.module('me.lazerka.orbit', [])
 	/**
 	 * All distances will be divided by this number so that we won't get rendering issues due to loss of precision.
-	 * For example, on 50m Kerbin and Mun are visible through each other.
+	 * For example, on 50,000,000m Kerbin and Mun are visible through each other.
+	 * Also, space box corners become visible.
 	 */
 	.constant('GLOBAL_SCALE', 1000000)
 ;
--- gae/src/main/webapp/smooth.js
+++ gae/src/main/webapp/smooth.js
@@ -1,8 +1,22 @@
 'use strict';
 angular.module('me.lazerka.orbit')
+	/**
+	 * Makes something to happen several times (every 20ms) over the course of `durationMs`.
+	 * Callback will be called with just one number [0, 1] -- how much of the task is done.
+	 *
+	 * Subsequent calls will cancel all scheduled runs for given `queue`.
+	 *
+	 * @param callback {Function}
+	 * @param queue {String} queue name.
+	 * @param durationMs {Number}
+	 * @param suppressDigest {Boolean?} whether to disable Angular dirty-checks on each call, default false.
+	 */
 	.factory('smooth', function($timeout) {
 		// Map of queue names to queue counter.
 		var queues = {};
+
+		// 4ms minimum per HTML5 spec.
+		var BETWEEN_MS = 20;
 
 		function run(callback, d, queue, expectedCounter) {
 			if (expectedCounter != queues[queue]) {
@@ -15,35 +29,23 @@ angular.module('me.lazerka.orbit')
 			callback(smoothed);
 		}
 
-		/**
-		 * Makes something to happen `ticksNum` times over course of `durationMs`.
-		 * Callback will be called with one number [0, 1] how much of the task is done.
-		 *
-		 * Subsequent calls will cancel all scheduled runs for given `queue`.
-		 * @param callback {Function}
-		 * @param queue {String} queue name.
-		 * @param durationMs {Number}
-		 * @param suppressDigest {Boolean?} whether to disable Angular dirty-checks on each call, default false.
-		 */
-		function enqueue(callback, queue, durationMs, suppressDigest) {
-			if (durationMs <= 20) {
-				throw new Error('durationMs must be more than 20ms');
-			}
-			var ticksNum = Math.ceil(durationMs / 20);
-
-			queues[queue] = queues[queue] || 0;
-			queues[queue]++;
-
-			// <= to
-			for (var tick = 1; tick <= ticksNum ; tick++) {
-				var d = tick / ticksNum;
-				var f = run.bind(this, callback, d, queue, queues[queue]);
-				$timeout(f, d * durationMs, !suppressDigest);
-			}
-		}
-
 		return {
-			enqueue: enqueue
+			enqueue: function (callback, queue, durationMs, suppressDigest) {
+				if (durationMs <= 20) {
+					throw new Error('durationMs must be more than 20ms');
+				}
+
+				var ticksNum = Math.ceil(durationMs / BETWEEN_MS);
+
+				queues[queue] = queues[queue] || 0;
+				queues[queue]++;
+
+				for (var tick = 1; tick <= ticksNum ; tick++) {
+					var d = tick / ticksNum;
+					var f = run.bind(this, callback, d, queue, queues[queue]);
+					$timeout(f, d * durationMs, !suppressDigest);
+				}
+			}
 		}
 	})
 ;
